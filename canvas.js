6c1f58ff9f55799507b11c091934115871a6d5ce
unknown <con40020@lib262511.ttu.edu>
2015-06-05 16:29:03 -0500

    more

--- canvas.js
+++ canvas.js
@@ -9,3 +9,40 @@ var ctx = canvas.getContext('2d');
 canvas.addEventListener('mousedown', startDraw, false);
 canvas.addEventListener('mousemove', draw, false);
 canvas.addEventListener('mouseup', endDraw, false);
+
+var isActive = false;
+
+var plots = [];
+
+function draw(e){
+	if(!isActive) {
+		return;
+	}
+	
+	var x = e.offsetX || e.layerX - canvas.offsetLeft;
+	var y = e.offsetY || e.layerY - canvas.offsetTop;
+	
+	plots.push({x: x, y: y});
+	
+	drawOnCanvas(plots);
+}
+
+function drawOnCanvas(color, plots) {
+	ctx.beginPath();
+	ctx.moveTo(plots[0].x, plots.[0].y);
+	
+	var i;
+	for(i = 1; i < plots.length; i++){
+		ctx.lineTo(plots[i].x, plots[i].y);
+	}
+	ctx.stroke();
+}
+
+function startDraw(e) {
+	isActive = true;
+}
+
+function endDraw(e) {
+	isActive = false;
+	plots = [];
+}
