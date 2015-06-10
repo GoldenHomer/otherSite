bb47f62357db95ae664e6aeb486dc12080567850
unknown <con40020@lib262511.ttu.edu>
2015-06-10 16:56:41 -0500

    changes

--- canvas.js
+++ canvas.js
@@ -1,14 +1,20 @@
+var PUBNUB_demo = PUBNUB.init({ 
+		publish_key: 'pub-c-6cc4f53c-edcc-41dc-96a5-976244127335', 
+		subscribe_key: 'sub-c-a6f5a5ee-0bbe-11e5-8990-02ee2ddab7fe' // Ain't much to do with these keys
+});
+
 var canvas = document.getElementById('drawCanvas');
 var ctx = canvas.getContext('2d');
 
 ctx.lineWidth = '3';
+ctx.lineCap = 'round';
+ctx.lineJoin = 'round';
 
 canvas.addEventListener('mousedown', startDraw, false);
 canvas.addEventListener('mousemove', draw, false);
 canvas.addEventListener('mouseup', endDraw, false);
 
 var isActive = false;
-
 var plots = [];
 
 function draw(e){
